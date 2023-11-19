ec447243aa5549592e1b43a8e980bb2db6f8a68b
USER2 <yidaqi113@outlook.com>
2023-11-19 13:12:08 +0800

    commit main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cin>>number;
+    if(number < 1 && number > 9)
+    {
+        cout<<"k1k1k1"<<endl;
+    }
+    if(number < 1 || number > 9)
+    {
+        cout<<"k2k2k2"<<endl;
+    }
+}
